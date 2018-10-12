4337daf442aa6d894e4104e664407496a383b577
Clinton Owen <clint@clintonowen.com>
2018-10-12 12:08:52 -0500

    Create async actions to GET and DELETE pets

---
+++ src/actions/dog.js
@@ -0,0 +1,44 @@
+import {API_BASE_URL} from '../config';
+
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS';
+export const fetchDogSuccess = dog => ({
+  type: FETCH_DOG_SUCCESS,
+  dog
+});
+
+export const ADOPT_DOG_SUCCESS = 'ADOPT_DOG_SUCCESS';
+export const adoptDogSuccess = () => ({
+  type: ADOPT_DOG_SUCCESS
+});
+
+export const fetchDog = () => dispatch => {
+  fetch(`${API_BASE_URL}/dog`)
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(dog => {
+      dispatch(fetchDogSuccess(dog));
+    });
+};
+
+export const adoptDog = () => dispatch => {
+  fetch(`${API_BASE_URL}/dog`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  })
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      // return res.json();
+    })
+    .then(() => {
+      dispatch(adoptDogSuccess());
+      dispatch(fetchDog());
+    });
+};
---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import {
+  fetchCatSuccess,
+  adoptCatSuccess,
+  fetchCat,
+  adoptCat
+} from './cat';
+
+import {
+  fetchDogSuccess,
+  adoptDogSuccess,
+  fetchDog,
+  adoptDog
+} from './dog';
+
+export default {
+  fetchCatSuccess,
+  adoptCatSuccess,
+  fetchCat,
+  adoptCat,
+  fetchDogSuccess,
+  adoptDogSuccess,
+  fetchDog,
+  adoptDog
+}
