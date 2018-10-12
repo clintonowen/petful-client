56b14cf7006a7820bf749ac7254ed962b6e04376
Clinton Owen <clint@clintonowen.com>
2018-10-12 11:19:19 -0500

    Add responsive grid and styles to Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,49 +7,51 @@ class Dashboard extends Component {
     const dog = this.props.dogToAdopt;
     return (
       <div className="Dashboard">
-        <header>
+        <header className="Dash-header">
           <h1>Welcome to Petful</h1>
         </header>
 
-        <section>
-          <header>
-            <h2>{cat.name}</h2>
-            <img src={cat.imageURL} alt={cat.imageDescription} />
-          </header>
-          <main>
-            <dl>
-              <dt>Age:</dt>
-              <dd>{cat.age}</dd>
-              <dt>Breed:</dt>
-              <dd>{cat.breed}</dd>
-              <dt>Sex:</dt>
-              <dd>{cat.sex}</dd>
-              <dt>Story:</dt>
-              <dd>{cat.story}</dd>
-            </dl>
-            <button>Adopt</button>
-          </main>
-        </section>
+        <div className="grid">
+          <section className="pet-info column">
+            <header>
+              <h2>{cat.name}</h2>
+              <img src={cat.imageURL} alt={cat.imageDescription} />
+            </header>
+            <main>
+              <dl>
+                <dt>Age:</dt>
+                <dd>{cat.age}</dd>
+                <dt>Breed:</dt>
+                <dd>{cat.breed}</dd>
+                <dt>Sex:</dt>
+                <dd>{cat.sex}</dd>
+                <dt>Story:</dt>
+                <dd>{cat.story}</dd>
+              </dl>
+              <button>Adopt</button>
+            </main>
+          </section>
 
-        <section>
-          <header>
-            <h2>{dog.name}</h2>
-            <img src={dog.imageURL} alt={dog.imageDescription} />
-          </header>
-          <main>
-            <dl>
-              <dt>Age:</dt>
-              <dd>{dog.age}</dd>
-              <dt>Breed:</dt>
-              <dd>{dog.breed}</dd>
-              <dt>Sex:</dt>
-              <dd>{dog.sex}</dd>
-              <dt>Story:</dt>
-              <dd>{dog.story}</dd>
-            </dl>
-            <button>Adopt</button>
-          </main>
-        </section>
+          <section className="pet-info column">
+            <header>
+              <h2>{dog.name}</h2>
+              <img src={dog.imageURL} alt={dog.imageDescription} />
+            </header>
+            <main>
+              <dl>
+                <dt>Age:</dt>
+                <dd>{dog.age}</dd>
+                <dt>Breed:</dt>
+                <dd>{dog.breed}</dd>
+                <dt>Sex:</dt>
+                <dd>{dog.sex}</dd>
+                <dt>Story:</dt>
+                <dd>{dog.story}</dd>
+              </dl>
+              <button>Adopt</button>
+            </main>
+          </section>
+        </div>
       </div>
     );
   }
